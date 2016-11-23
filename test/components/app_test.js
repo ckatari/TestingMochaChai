e89f849f06989ea0465bf653c6d074aa42a51261
ck <ck1956@nyu.edu>
2016-11-23 13:54:08 -0500

    test2

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,20 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// Use 'describe' to group together similar tests
+describe('App', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
+
+    // create an instance of App
+    const component = renderComponent(App); 
+
+    // Use 'expect' to make an assertion about a target
+    expect(component).to.contain('React simple starter');
+
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
+
+
 });
